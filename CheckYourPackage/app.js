2bafaf8c88a5a9a7dd0a2cbb165e526d16e396e8
梓榳(玥餅) <SherryYue.Chiu@gmail.com>
2020-11-27 20:11:36 +0800

    create CheckYourPackage

---
+++ CheckYourPackage/app.js
@@ -0,0 +1,44 @@
+lastUpdateBlock = document.querySelector("#lastUpdateBlock");
+container = document.querySelector("#packageContainer");
+packagesBlock = document.querySelectorAll(".packages");
+
+function showPackages() {
+    //to package array
+    packages = packages.trim().split(/[\r\n]{2,}/);
+
+    //to package-attribute array
+    packages = packages.map(package => package.split(/[\r\n]+/));
+    packages.map(package => {
+        package.map(attribute => {
+            if (attribute.indexOf("From:") != -1) {
+                package.from = attribute.substr(5);
+            } else if (attribute.indexOf("To:") != -1) {
+                package.to = attribute.substr(3);
+            } else if (attribute.indexOf("Room:") != -1) {
+                package.room = attribute.substr(5);
+            } else if (attribute.indexOf("Color:") != -1) {
+                package.color = attribute.substr(6);
+            }
+            return attribute;
+        });
+        return package;
+    });
+
+    //show DOM
+    packages.forEach(o => {
+        let html = `
+<div class="package">
+    ${o.to?"<span class='to'>"+o.to+"</span>":""}
+    ${o.from?"<span class='from'>"+o.from+"</span>":""}
+    ${o.room?"<span class='room'>"+o.room+"</span>":""}
+    ${o.color?"<span class='color'>"+o.color+"</span>":""}
+</div>
+    `;
+        container.insertAdjacentHTML("beforeend", html);
+    });
+}
+window.onload = function() {
+    lastUpdateBlock.innerHTML = lastUpdate;
+    showPackages();
+
+};
---
+++ CheckYourPackage/data.js
@@ -0,0 +1,20 @@
+lastUpdate = "~ 2020/11/27";
+packages = `
+To:張秀娟
+Room:3F
+From:郵局招領單
+Color:白色
+
+To:陳奐如
+Room:305
+From:國泰世華
+Color:白色
+
+To:陳奐如
+Room:305
+Color:褐色
+
+To:吳韻哲
+From:就服處
+Color:藍色x2、綠色x1
+`;

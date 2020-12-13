f2b7bd95b09aff005fb34473571f88e7abafe47e
梓榳(玥餅) <sherryyue.c@protonmail.com>
2020-12-13 10:06:34 +0800

    update letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,36 +1,8 @@
-lastUpdate = "~ 2020/12/10 23PM";
+lastUpdate = "~ 2020/12/13 11AM";
 packages = `
 To:許甄霙
 From:天下雜誌
 Room:304
-Color:白色
-
-To:許呈祥
-From:天下雜誌
-Color:白色
-
-To:葉秋伶
-From:相聲瓦舍
-Room:503
-Color:白色
-
-To:康書偉
-From:華南銀行
-Room:510
-Color:白色
-
-To:吳美環
-From:全聯DM
-Color:白色
-
-To:邱耀陞
-From:勞保局
-Room:507
-Color:白色
-
-To:張秀娟
-Room:3F
-From:郵局招領單
 Color:白色
 
 To:陳奐如

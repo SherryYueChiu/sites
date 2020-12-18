99f5c2d161ee98cadf27cc577a792ded1e8dd42b
梓榳(玥餅) <sherryyue.c@protonmail.com>
2020-12-18 17:21:55 +0800

    updare letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,20 +1,5 @@
-lastUpdate = "~ 2020/12/17 19PM";
+lastUpdate = "~ 2020/12/18 19PM";
 packages = `
-To:YuShiang Chen
-From:Church of Scientology Flag Service Organization
-
-To:許呈祥
-From:東海大學刊物
-
-To:陳靜如
-From:郵局招領單
-
-To:謝有惟
-From:全聯DM
-
-To:吳美環
-From:全聯DM
-
 To:陳奐如
 Room:305
 From:國泰世華

ba6bb72c8f2d48c0342d7c728724dc1e9c531775
梓榳(玥餅) <sherryyue.c@protonmail.com>
2020-12-20 19:46:16 +0800

    update letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,4 +1,4 @@
-lastUpdate = "~ 2020/12/21 18PM";
+lastUpdate = "~ 2020/12/20 20PM";
 packages = `
 To:陳奐如
 Room:305
@@ -8,8 +8,4 @@ Color:白色
 To:陳奐如
 Room:305
 Color:褐色
-
-To:吳韻哲
-From:就服處
-Color:藍色x2、綠色x1、黃色x1
 `;

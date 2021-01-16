c87e1d6c97040db630f40543bd1648dc3a1c58e7
梓榳(玥餅) <sherryyue.c@protonmail.com>
2021-01-16 19:44:26 +0800

    update letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,13 +1,5 @@
-lastUpdate = "~ 2021/01/13 05AM";
+lastUpdate = "~ 2021/01/16 21PM";
 packages = `
-To:吳美環
-From:台灣人壽
-Color:白色
-
-To:Yu-Hsiang Shen
-From:山達基
-Color:白色
-
 To:馬智宣
 From:童綜合醫院
 Color:白色

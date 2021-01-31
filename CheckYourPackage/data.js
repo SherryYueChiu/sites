f05fa3ed0295993368bef65e98f6c1815ff8e09c
梓榳(玥餅) <sherryyue.c@protonmail.com>
2021-01-31 18:39:48 +0800

    update letter

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,5 +1,15 @@
-lastUpdate = "~ 2021/01/29 14PM";
+lastUpdate = "~ 2021/01/31 20PM";
 packages = `
+To:陳昱廷
+From:聖安宮
+Color:紅色
+Room:505
+
+To:吳怡瑾
+From:廖如
+Color:卡片
+Room:307
+
 To:陳英和
 From:郵寄招領
 Color:白色
@@ -8,4 +18,5 @@ Room:307
 To:陳昱廷
 From:玉山銀行
 Color:白色
+Room:505
 `;

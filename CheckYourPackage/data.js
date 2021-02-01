c314172e3cd863aac88779e0e2509f34129672f9
梓榳(玥餅) <sherryyue.c@protonmail.com>
2021-02-01 23:58:23 +0800

    upload letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,5 +1,24 @@
-lastUpdate = "~ 2021/01/31 20PM";
+lastUpdate = "~ 2021/02/01 04AM";
 packages = `
+To:邱耀陞
+From:勞保局
+Color:白色
+Room:507
+
+To:林鼎強
+From:東海大學註冊組
+Color:白色
+Room:407
+
+To:劉天仲
+From:東海大學註冊組
+Color:白色
+Room:204
+
+To:馬智宣
+From:聖安宮
+Color:紅色
+
 To:陳昱廷
 From:聖安宮
 Color:紅色

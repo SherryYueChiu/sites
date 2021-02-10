56f2f657157fe959c8507c9fce5621706545af88
梓榳(玥餅) <sherryyue.c@protonmail.com>
2021-02-10 23:46:02 +0800

    update letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,10 +1,5 @@
-lastUpdate = "~ 2021/02/04 04AM";
+lastUpdate = "~ 2021/02/11 06AM";
 packages = `
-To:邱耀陞
-From:勞保局
-Color:白色
-Room:507
-
 To:林鼎強
 From:東海大學註冊組
 Color:白色
@@ -16,26 +11,15 @@ Color:白色
 Room:204
 
 To:馬智宣
+From:東海大學註冊組
+Color:白色
+
+To:馬智宣
 From:聖安宮
 Color:紅色
-
-To:陳昱廷
-From:聖安宮
-Color:紅色
-Room:505
-
-To:吳怡瑾
-From:廖如
-Color:卡片
-Room:307
 
 To:陳英和
 From:郵寄招領
 Color:白色
 Room:307
-
-To:陳昱廷
-From:玉山銀行
-Color:白色
-Room:505
 `;

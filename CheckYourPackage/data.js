97040b88315604938bccfcfb3b3b5ffda719188d
梓榳(玥餅) <sherryyue.c@protonmail.com>
2021-02-14 00:16:25 +0800

    update time

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,4 +1,4 @@
-lastUpdate = "~ 2021/02/12 06AM";
+lastUpdate = "~ 2021/02/14 06AM";
 packages = `
 To:林鼎強
 From:東海大學註冊組
@@ -13,10 +13,6 @@ Room:204
 To:馬智宣
 From:東海大學註冊組
 Color:白色
-
-To:馬智宣
-From:聖安宮
-Color:紅色
 
 To:陳英和
 From:郵寄招領

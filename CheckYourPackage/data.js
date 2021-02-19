3c4b62c7df01b87a12641f33d262d06511e608bc
梓榳(玥餅) <sherryyue.c@protonmail.com>
2021-02-19 19:40:03 +0800

    upload degree

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,5 +1,9 @@
 lastUpdate = "~ 2021/02/20 06AM";
 packages = `
+To:吳美環
+From:玉山銀行
+Color:白色
+
 To:林鼎強
 From:東海大學註冊組
 Color:白色
@@ -12,5 +16,9 @@ Room:204
 
 To:馬智宣
 From:東海大學註冊組
+Color:白色
+
+To:馬智宣
+From:童綜合醫院
 Color:白色
 `;

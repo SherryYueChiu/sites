2146c5145646251c3eed2b212ee0eaa050dcf780
Sherry <sherryyue.chiu@protonmail.com>
2021-02-23 20:08:25 +0800

    publish THUAudit for 109-2

--- THUAudit/js/classCrawer.js
+++ THUAudit/js/classCrawer.js
@@ -1,36 +1,38 @@
-  cnt = $(".button_hilite").length;
-  //json = `[`;
-  json = "";
+var classCnt = $(".button_hilite").length;
+var json = "";
 
-  for (i = 0; i < cnt; i++) {
+for (i = 0; i < classCnt; i++) {
+    var ccode = "",
+        cname = "",
+        croom = "",
+        ctime = "";
 
-    //課碼
-    code = $(".button_hilite").eq(i).html().trim();
+    ccode = $(".button_hilite").eq(i).html().trim(); //課碼
 
     //課名
     if ($("[data-title=課程名稱]").eq(i).html().indexOf("strike") == -1) {
-      name = $("[data-title=課程名稱]>a").eq(i).html().trim();
+        nacnameme = $("[data-title=課程名稱]>a").eq(i).html().trim();
     } else {
-      //已停開
-      continue;
+        //已停開
+        continue;
     }
 
     //時間地點
     timeroom = $("[data-title=時間地點]").eq(i).html().trim();
     if (timeroom.indexOf("星期") != -1) {
-      room = timeroom.split("[")[1]?.replace("]", "") || "";
-      time = timeroom.split("[")[0]?.replace("星期", "")?.split(/[,|\/]/) || "";
-      time = `["${time?.join('","')}"]`
+        croom = timeroom.split("[")[1] ? .replace("]", "") || "";
+        ctime = timeroom.split("[")[0] ? .replace("星期", "") ? .split(/[,|\/]/) || "";
+        ctime = `["${ctime?.join('","')}"]`
     } else {
-      //無公佈時間地點
-      continue;
+        //無公佈時間地點
+        continue;
     }
 
     json += `{
-          "code": "${code}",
-          "name": "${name}",
-          "room": "${room}",
-          "time": ${time}
+          "code": "${ccode}",
+          "name": "${cname}",
+          "room": "${croom}",
+          "time": ${ctime}
   },`;
-  }json
-  //json += `]`;
+}
+console.log(json);

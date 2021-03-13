0ba4e3cc9f803f3d21ae5c1e03d7add9929adcde
Sherry <sherryyue.c@protonmail.com>
2021-03-14 00:12:20 +0800

    update letters' descriptions

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -2,33 +2,33 @@ lastUpdate = "~ 2021/03/14 06AM";
 packages = `
 To:鍾筱薇
 From:中國信託
-Color:白色
+Color:白色信封
 Room:207
 
 To:康書偉
-From:華南銀行
-Color:白色
+From:華南銀行信封
+Color:白色信封
 Room:510
 
 To:林鼎強
-Color:黑色
+Color:黑色信封
 Room:403
 
 To:吳蘊哲
 From:郵寄招領：彰化銀行
-Color:白色
+Color:白色信封
 Room:501
 
 To:劉天仲
 From:東海大學註冊組
-Color:白色
+Color:白色信封
 Room:204
 
 To:馬智宣
 From:東海大學註冊組
-Color:白色
+Color:白色信封
 
 To:馬智宣
 From:童綜合醫院
-Color:白色
+Color:白色單張
 `;

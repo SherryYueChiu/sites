166b10096dbad2f57a805dcfcb1a1a8e152fc449
Sherry <sherryyue.c@protonmail.com>
2021-03-27 15:06:31 +0800

    store and restore unfinished tour

--- HowFarStill/app.js
+++ HowFarStill/app.js
@@ -1,20 +1,19 @@
 //variables
-var watcher; //Listening GeoLocation
-var wakeLock = null; //screen wake
-var start = { lat: 0, lng: 0 };
-var target = { lat: 0, lng: 0 };
-var totalDistance = 0;
-var distanceColor = [
+watcher = null; //Listening GeoLocation
+fromLocation = { lat: 0, lng: 0 };
+targetLocation = { lat: 0, lng: 0 };
+totalDistance = 0;
+distanceColor = [
     "#FDE8E9",
     "#E3BAC6",
     "#BC9EC1",
     "#596475",
     "#1F2232"
 ]
-var data_storedPlaces = "";
+data_storedPlaces = "";
 
 //DOMs
-var root = document.getElementsByTagName('html')[0];
+root = document.getElementsByTagName('html')[0];
 startTrackingDistance = document.getElementById("startTrackingDistance");
 targetGeo = document.getElementById("targetGeo");
 distanceDisplay = document.getElementById("distanceDisplay");
@@ -29,32 +28,22 @@ function toRad(Value) {
 }
 
 function calcDistance() {
-    lat1 = start.lat;
-    lon1 = start.lng;
-    lat2 = target.lat;
-    lon2 = target.lng;
-    var R = 6371; // km
-    var dLat = toRad(lat2 - lat1);
-    var dLon = toRad(lon2 - lon1);
-    var lat1 = toRad(lat1);
-    var lat2 = toRad(lat2);
+    let lat1 = fromLocation.lat;
+    let lon1 = fromLocation.lng;
+    let lat2 = targetLocation.lat;
+    let lon2 = targetLocation.lng;
+    const R = 6371; // km
+    dLat = toRad(lat2 - lat1);
+    dLon = toRad(lon2 - lon1);
+    lat1 = toRad(lat1);
+    lat2 = toRad(lat2);
 
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
     return d;
 }
-
-const requestWakeLock = async() => {
-    try {
-        const wakeLock = await navigator.wakeLock.request('screen');
-    } catch (err) {
-        // The wake lock request fails - usually system-related, such as low battery.
-        console.log(`${err.name}, ${err.message}`);
-    }
-}
-requestWakeLock();
 
 function distanceText(distance) {
     if (distance > 10) return ">10km";
@@ -74,14 +63,12 @@ function isGeolocationAvailable() {
     }
 }
 
-//test: 24.1814342, 120.5888504
 function getCurrentPosition() {
     navigator.geolocation.getCurrentPosition(function(position) {
-        let lat, lng;
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
-        start.lat = lat;
-        start.lng = lng;
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        fromLocation.lat = lat;
+        fromLocation.lng = lng;
         if (lat == 0 && lng == 0) {
             nowGeo.innerHTML = "請開啟GPS";
         }
@@ -92,11 +79,10 @@ function getCurrentPosition() {
 function startTrackingMyLocation() {
     navigator.geolocation.clearWatch(watcher);
     watcher = navigator.geolocation.watchPosition(function(position) {
-        let lat, lng;
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
-        start.lat = lat;
-        start.lng = lng;
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        fromLocation.lat = lat;
+        fromLocation.lng = lng;
         nowGeo.innerHTML = `${lat.toFixed(7)},${lng.toFixed(7)}`;
         startTrackingDisTance();
     });
@@ -117,14 +103,15 @@ function startTrackingDisTance() {
 
 function initStoredPlaces() {
     data_storedPlaces = localStorage.getItem("places");
+    if(!data_storedPlaces)  return;
     listDOM = document.querySelector("#selectPlaceStored ul");
     list = data_storedPlaces.trim().split(/[\r\n]+/);
     listDOM.innerHTML = "";
     list.forEach((cur, i, arr) => {
         //隔行存
         if (i % 2 != 0) {
-            geo = cur;
-            place = arr[i - 1];
+            const geo = cur;
+            const place = arr[i - 1];
             listDOM.innerHTML += `<li geo="${geo}">${place}</li>`;
         }
     });
@@ -146,26 +133,28 @@ startTrackingDistance.addEventListener("click", () => {
         if (targetGeo.value) {
             if (targetGeo.value.match(/[\d.]+, *[\d.]+/)) {
                 //format string: lat, lng
-                let regex = targetGeo.value.match(/([\d.]+), *([\d.]+)/);
-                target.lat = 1 * regex[1];
-                target.lng = 1 * regex[2];
+                const regex = targetGeo.value.match(/([\d.]+), *([\d.]+)/);
+                targetLocation.lat = 1 * regex[1];
+                targetLocation.lng = 1 * regex[2];
                 getCurrentPosition();
                 startTrackingMyLocation();
                 //toggle buttons
                 startTrackingDistance.style.display = "none";
                 menuBtn.style.display = "none";
                 pauseBtn.style.display = "block";
+                localStorage.setItem("targetLocation", `${targetLocation.lat}, ${targetLocation.lng}`);
             } else if (targetGeo.getAttribute("realGeo").match(/[\d.]+, *[\d.]+/)) {
                 //format string: lat, lng
-                let regex = targetGeo.getAttribute("realGeo").match(/([\d.]+), *([\d.]+)/);
-                target.lat = 1 * regex[1];
-                target.lng = 1 * regex[2];
+                const regex = targetGeo.getAttribute("realGeo").match(/([\d.]+), *([\d.]+)/);
+                targetLocation.lat = 1 * regex[1];
+                targetLocation.lng = 1 * regex[2];
                 getCurrentPosition();
                 startTrackingMyLocation();
                 //toggle buttons
                 startTrackingDistance.style.display = "none";
                 menuBtn.style.display = "none";
                 pauseBtn.style.display = "block";
+                localStorage.setItem("targetLocation", `${targetLocation.lat}, ${targetLocation.lng}`);
             } else {
                 alert("請輸入正確的格式：經度, 緯度");
             }
@@ -184,6 +173,7 @@ document.querySelector(".container").addEventListener("click", () => {
 //pause button
 pauseBtn.addEventListener("click", () => {
     if (confirm("確定要結束？")) {
+        localStorage.setItem("targetLocation", "");
         history.go(0);
     }
 });
@@ -212,7 +202,7 @@ document.querySelector("#selectPlaceStored .dismiss").addEventListener("click", 
 
 //in storePlaceModal
 document.querySelector("#storePlaceModal .confirm").addEventListener("click", async() => {
-    localStorage.setItem("places", document.querySelector("#inputPlaces").value.trim());
+    localStorage.setItem("places", document.querySelector("#inputPlaces").value.trim()??"");
     storePlaceModal.style.display = "none";
     await initStoredPlaces();
     //bind events
@@ -226,14 +216,21 @@ document.querySelector("#storePlaceModal .confirm").addEventListener("click", as
 });
 
 document.querySelector("#storePlaceModal .cancel").addEventListener("click", () => {
-    document.querySelector("#inputPlaces").value = localStorage.getItem("places");
+    document.querySelector("#inputPlaces").value = localStorage.getItem("places")??"";
     storePlaceModal.style.display = "none";
 });
 
 async function onInit() {
-    data_storedPlaces = localStorage.getItem("places").trim();
-    document.querySelector("#inputPlaces").value = data_storedPlaces;
+    data_storedPlaces = localStorage.getItem("places")?.trim();
+    document.querySelector("#inputPlaces").value = data_storedPlaces??"";
     await initStoredPlaces();
+    //restore previous state
+    if(!!localStorage.getItem("targetLocation")){
+        let storedLocation = {};
+        [storedLocation.lat, storedLocation.lng] = localStorage.getItem("targetLocation").split(",");
+        targetGeo.value = `${storedLocation.lat}, ${storedLocation.lng}`;
+        startTrackingDistance.click();
+    }
     //bind events
     document.querySelectorAll("#selectPlaceStored li").forEach((element) => {
         element.addEventListener("click", (event) => {
@@ -244,14 +241,12 @@ async function onInit() {
     });
     //update current position
     watcher = navigator.geolocation.watchPosition(function(position) {
-        let lat, lng;
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
-        start.lat = lat;
-        start.lng = lng;
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        fromLocation.lat = lat;
+        fromLocation.lng = lng;
         nowGeo.innerHTML = `${lat.toFixed(7)},${lng.toFixed(7)}`;
     });
-    requestWakeLock();
 }
 
 onInit();

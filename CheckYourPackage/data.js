f31508cb34a66e62e733362d63f4811f0e05aa3e
Sherry <sherryyue.c@protonmail.com>
2021-04-02 01:00:47 +0800

    upload letter

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,15 +1,10 @@
-lastUpdate = "~ 2021/04/01 6AM";
+lastUpdate = "~ 2021/04/02 6AM";
 packages = `
 
 To:陳昱廷
 From:玉山銀行
 Color:白色信封
 Room:505
-
-To:邱耀陞
-From:勞保局
-Color:白色信封
-Room:507
 
 To:鍾筱薇
 From:中國信託

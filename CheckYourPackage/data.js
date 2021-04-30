9914eae6b0a3fbf2f3bd4cbf240895fe2c9279fe
Sherry <sherryyue.c@protonmail.com>
2021-04-30 17:01:12 +0800

    update letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,14 +1,14 @@
-lastUpdate = "~ 2021/04/28 6AM";
+lastUpdate = "~ 2021/04/29 6AM";
 packages = `
-To:陳靜如
-From:東震
+To:陳昱廷
+From:玉山銀行
 Color:白色信封
-Room:504
+Room:505
 
-To:陳玉琳
-From:台積電
+To:陳昱廷
+From:南山人壽
 Color:白色信封
-Room:306
+Room:505
 
 To:吳蘊哲
 From:彰化銀行

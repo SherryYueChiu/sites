7a9ccc9060fd014b48c928fde7d88e9737e902b2
SherryYueChiu <sherryyue.c@protonmail.com>
2021-05-02 01:36:18 +0800

    update letters

--- CheckYourPackage/data.js
+++ CheckYourPackage/data.js
@@ -1,21 +1,4 @@
-lastUpdate = "~ 2021/04/29 6AM";
+lastUpdate = "~ 2021/05/02 6AM";
 packages = `
-To:陳昱廷
-From:玉山銀行
-Color:白色信封
-Room:505
 
-To:陳昱廷
-From:南山人壽
-Color:白色信封
-Room:505
-
-To:吳蘊哲
-From:彰化銀行
-Color:白色信封
-Room:204
-
-To:馬智宣
-From:童綜合醫院
-Color:白色單張
 `;
